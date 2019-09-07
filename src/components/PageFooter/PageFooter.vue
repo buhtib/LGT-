8fec7c3f980d8703bba4aa3e12f376c277d55150
buhtib <2418531916@qq.com>
2019-09-07 18:00:25 +0800

    update 格式化下首页的代码

--- src/components/PageFooter/PageFooter.vue
+++ src/components/PageFooter/PageFooter.vue
@@ -1,119 +1,150 @@
 <!-- 公共底部  -->
 <template>
-    <div class="page-footer bg2c colorccc font12">
-        <div class="section d-flex">
-            <div class="page-footer-section d-flex align-items-center  justify-content-between">
-                <div class="pic ">
-                    <img v-lazy="require('../../assets/img/logo.png')" alt="hehe"  />
-                </div>
-
-                 <ul class="service clearfix">
-                    <div class="mr-2 mb-2 ">服务范围</div>
-                    <li v-for="(item, index) in serviceList" :key="index" class="mb-1 mr-1">
-                        <span>{{item}}</span>
-                    </li>
-                   
-                </ul>
-
-                <ul class="contact ">
-                    <li class="d-flex mb-2">
-                        <div class="icon">
-                            <img src="~@/assets/img/email-white.png" alt="" srcset="">
-                            <span class="separation-line">|</span>
-                        </div>
-                        <span>1019114350@qq.com</span>
-                    </li>
-                    <li class="d-flex mb-2">
-                        <div class="icon"> 
-                            <img src="~@/assets/img/tel-white.png" alt="" srcset="">
-                            <span class="separation-line">|</span>
-                        </div>
-                        <span>18219082540</span>
-                    </li>
-                    <li class="d-flex">
-                        <div class="icon">
-                            <img src="~@/assets/img/address-white.png" alt="" srcset="">
-                            <span class="separation-line">|</span>
-                        </div>
-                        <span>深圳市龙华区民治樟坑一区129栋</span>
-                    </li>
-                </ul>
-
-
-                <a-row class="qr-code h-100" type="flex"  justify="center" align="middle">
-                    <a-col :span="12" class=" d-flex flex-column justify-content-center align-items-center pr-5" >
-                        <div class="">
-                            <img v-lazy="require('../../assets/img/gw-qrcode.png')" alt="hehe"  />
-                        </div>
-                        <span class="mt-1">资深品牌顾问</span>
-                    </a-col>
-                    <a-col :span="12" class=" d-flex flex-column justify-content-center align-items-center" >
-                        <div class="">
-                            <img v-lazy="require('../../assets/img/wx-qrcode.png')" alt="hehe"  />
-                        </div>
-                        <span class="mt-1">LGT公众号</span>
-                    </a-col>
-                </a-row>
-            </div>
-
+  <div class="page-footer bg2c colorccc font12">
+    <div class="section d-flex">
+      <div
+        class="page-footer-section d-flex align-items-center  justify-content-between"
+      >
+        <div class="pic ">
+          <img v-lazy="require('../../assets/img/logo.png')" alt="hehe" />
         </div>
+
+        <ul class="service clearfix">
+          <div class="mr-2 mb-2 ">服务范围</div>
+          <li
+            v-for="(item, index) in serviceList"
+            :key="index"
+            class="mb-1 mr-1"
+          >
+            <span>{{ item }}</span>
+          </li>
+        </ul>
+
+        <ul class="contact ">
+          <li class="d-flex mb-2">
+            <div class="icon">
+              <img src="~@/assets/img/email-white.png" alt="" srcset="" />
+              <span class="separation-line">|</span>
+            </div>
+            <span>1019114350@qq.com</span>
+          </li>
+          <li class="d-flex mb-2">
+            <div class="icon">
+              <img src="~@/assets/img/tel-white.png" alt="" srcset="" />
+              <span class="separation-line">|</span>
+            </div>
+            <span>18219082540</span>
+          </li>
+          <li class="d-flex">
+            <div class="icon">
+              <img src="~@/assets/img/address-white.png" alt="" srcset="" />
+              <span class="separation-line">|</span>
+            </div>
+            <span>深圳市龙华区民治樟坑一区129栋</span>
+          </li>
+        </ul>
+
+        <a-row
+          class="qr-code h-100"
+          type="flex"
+          justify="center"
+          align="middle"
+        >
+          <a-col
+            :span="12"
+            class=" d-flex flex-column justify-content-center align-items-center pr-5"
+          >
+            <div class="">
+              <img
+                v-lazy="require('../../assets/img/gw-qrcode.png')"
+                alt="hehe"
+              />
+            </div>
+            <span class="mt-1">资深品牌顾问</span>
+          </a-col>
+          <a-col
+            :span="12"
+            class=" d-flex flex-column justify-content-center align-items-center"
+          >
+            <div class="">
+              <img
+                v-lazy="require('../../assets/img/wx-qrcode.png')"
+                alt="hehe"
+              />
+            </div>
+            <span class="mt-1">LGT公众号</span>
+          </a-col>
+        </a-row>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "PageFooter",
-    data() {
-        return {
-            serviceList:['LOGO设计', '宣传册设计','x架设计','画册设计','易拉宝设计','包装设计','品牌VI手册设计','小程序开发','网站开发','APP开发']
-        }
-    },
+  name: "PageFooter",
+  data() {
+    return {
+      serviceList: [
+        "LOGO设计",
+        "宣传册设计",
+        "x架设计",
+        "画册设计",
+        "易拉宝设计",
+        "包装设计",
+        "品牌VI手册设计",
+        "小程序开发",
+        "网站开发",
+        "APP开发"
+      ]
+    };
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .page-footer {
-    height: 180px;
-    margin-top: 40px;
-    padding: 40px 0;
-    .section {
-        .page-footer-section{
-            width: 100%;
-            height: 100%;
-            .pic {
-                width: 130px;
-                height: 90px;
-                line-height: 80px;
-                text-align: center;
-                background-color: #fff;
-                border-radius: 4px;
-            }
+  height: 180px;
+  margin-top: 40px;
+  padding: 40px 0;
+  .section {
+    .page-footer-section {
+      width: 100%;
+      height: 100%;
+      .pic {
+        width: 130px;
+        height: 90px;
+        line-height: 80px;
+        text-align: center;
+        background-color: #fff;
+        border-radius: 4px;
+      }
 
-            .service {
-                width: 400px;
-                margin: 0;
-                >li {
-                    min-width: 24%;
-                    white-space: nowrap;
-                    float: left;
-                }
-            }
-
-            .contact {
-                margin: 0;
-                white-space: nowrap;
-                .icon {
-                    width: 50px;
-                    .separation-line {
-                        margin: 0 10px;
-                    }
-                }
-            }
-            
-            .qr-code {
-white-space: nowrap;
-            }
+      .service {
+        width: 400px;
+        margin: 0;
+        > li {
+          min-width: 24%;
+          white-space: nowrap;
+          float: left;
         }
+      }
+
+      .contact {
+        margin: 0;
+        white-space: nowrap;
+        .icon {
+          width: 50px;
+          .separation-line {
+            margin: 0 10px;
+          }
+        }
+      }
+
+      .qr-code {
+        white-space: nowrap;
+      }
     }
+  }
 }
 </style>
--- src/components/PageHeader/PageHeader.vue
+++ src/components/PageHeader/PageHeader.vue
@@ -1,121 +1,122 @@
 <!-- 公共头部  -->
 <template>
-    <div class="page-header" ref="container">
-        <a-affix :offsetTop="0.00000001">
-
-                <div class="page-header-intro d-flex justify-content-between section">
-                    <div class="page-header-intro_logo d-flex justify-content-center align-items-center">
-                        <img src="~@/assets/img/logo.png" alt="logo"  />
-                    </div>
-                    <ul
-                        class="page-header-intro_area d-flex flex-column align-items-center justify-content-center"
-                    >
-                        <li>
-                            <span class="line"></span>
-                            <span class="txt">用设计塑造品牌价值</span>
-                            <span class="line"></span>
-                        </li>
-                        <li class="d-flex">
-                            <div>
-                                <img src="~@/assets/img/email-blue.png" alt=""  >&nbsp;&nbsp; 
-                                <span>1019114350@qq.com &nbsp;&nbsp; &nbsp; &nbsp;</span>
-                            </div> 
-                            <div>
-                                <img src="~@/assets/img/wx-blue.png" alt=""  >&nbsp;&nbsp; 
-                                <img src="~@/assets/img/tel-blue.png" alt=""  >&nbsp;&nbsp; 
-                                <span>电话/微信：18219082540</span>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-
-            <div class="nav d-flex justify-content-center">
-                <div class="row align-items-center section">
-                    <router-link
-                        class="col-xl r-link"
-                        v-for="(tab, i) in navList"
-                        :key="i"
-                        :to="tab.path"
-                        exact
-                    >{{ tab.name }}</router-link>
-                </div>
+  <div class="page-header" ref="container">
+    <a-affix :offsetTop="0.00000001">
+      <div class="page-header-intro d-flex justify-content-between section">
+        <div
+          class="page-header-intro_logo d-flex justify-content-center align-items-center"
+        >
+          <img src="~@/assets/img/logo.png" alt="logo" />
+        </div>
+        <ul
+          class="page-header-intro_area d-flex flex-column align-items-center justify-content-center"
+        >
+          <li>
+            <span class="line"></span>
+            <span class="txt">用设计塑造品牌价值</span>
+            <span class="line"></span>
+          </li>
+          <li class="d-flex">
+            <div>
+              <img src="~@/assets/img/email-blue.png" alt="" />&nbsp;&nbsp;
+              <span>1019114350@qq.com &nbsp;&nbsp; &nbsp; &nbsp;</span>
             </div>
+            <div>
+              <img src="~@/assets/img/wx-blue.png" alt="" />&nbsp;&nbsp;
+              <img src="~@/assets/img/tel-blue.png" alt="" />&nbsp;&nbsp;
+              <span>电话/微信：18219082540</span>
+            </div>
+          </li>
+        </ul>
+      </div>
 
-        </a-affix>
-    </div>
+      <div class="nav d-flex justify-content-center">
+        <div class="row align-items-center section">
+          <router-link
+            class="col-xl r-link"
+            v-for="(tab, i) in navList"
+            :key="i"
+            :to="tab.path"
+            exact
+            >{{ tab.name }}</router-link
+          >
+        </div>
+      </div>
+    </a-affix>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "PageHeader",
-    data() {
-        return {
-            navList: [
-                { name: "首页", path: "/home" },
-                { name: "LGT创意", path: "/brand" },
-                { name: "服务范围", path: "/service" },
-                { name: "LGT项目", path: "/project" },
-                { name: "联系我们", path: "/linkus" }
-            ],
-        };
-    }
+  name: "PageHeader",
+  data() {
+    return {
+      navList: [
+        { name: "首页", path: "/home" },
+        { name: "LGT创意", path: "/brand" },
+        { name: "服务范围", path: "/service" },
+        { name: "LGT项目", path: "/project" },
+        { name: "联系我们", path: "/linkus" }
+      ]
+    };
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .page-header {
-    width: 100%;
+  width: 100%;
+  background-color: #fff;
+  /deep/ .ant-affix {
     background-color: #fff;
-    /deep/ .ant-affix {
-        background-color: #fff;
+  }
+  .page-header-intro {
+    height: 110px;
+    .page-header-intro_logo {
+      width: 30%;
+      height: 100%;
     }
-    .page-header-intro {
-        height: 110px;
-        .page-header-intro_logo {
-            width: 30%;
-            height: 100%;
-        }
-        .page-header-intro_area {
-            width: 40%;
-            height: 100%;
+    .page-header-intro_area {
+      width: 40%;
+      height: 100%;
 
-            > li {
-                &:first-of-type {
-                    display: flex;
-                    align-items: center;
-                    justify-content: space-between;
-                    width: 100%;
-                    font-size: 16px;
-                    color: #348ccd;
-                    .line {
-                        flex: 1;
-                        height: 1px;
-                        background-color: #348ccd;
-                    }
-                    .txt {
-                        margin: 0 8px;
-                    }
-                }
-                &:last-of-type {
-                    margin-top: 10px;
-                    font-size: 14px;
-                    color: #333;
-                }
-            }
+      > li {
+        &:first-of-type {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          width: 100%;
+          font-size: 16px;
+          color: #348ccd;
+          .line {
+            flex: 1;
+            height: 1px;
+            background-color: #348ccd;
+          }
+          .txt {
+            margin: 0 8px;
+          }
         }
+        &:last-of-type {
+          margin-top: 10px;
+          font-size: 14px;
+          color: #333;
+        }
+      }
     }
+  }
 
-    .nav {
-        background-color: #348ccd;
-        .row {
-            height: 40px;
-            font-size: 14px;
-            .col-xl {
-                padding: 10px 5px;
-                text-align: center;
-                color: #fff;
-            }
-        }
+  .nav {
+    background-color: #348ccd;
+    .row {
+      height: 40px;
+      font-size: 14px;
+      .col-xl {
+        padding: 10px 5px;
+        text-align: center;
+        color: #fff;
+      }
     }
+  }
 }
 </style>
