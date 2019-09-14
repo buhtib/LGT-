dd6aba04a9708508d1ddcddaf2eed4b97ba03d31
LuGuangZong <1454118276@qq.com>
2019-09-14 14:04:47 +0800

    'update更新项目页'

--- src/page/Project/Project.vue
+++ src/page/Project/Project.vue
@@ -3,34 +3,30 @@
   <div class="project">
     <div class="section">
       <div class="select-project font24">
-        <a-row
-          type="flex"
-          justify="center"
-          v-for="(row, index) in projectNameList"
-          :key="index"
-          class="my-5"
-        >
+        <a-row type="flex" justify="center" v-for="(row, index) in projectNameList" :key="index">
           <a-col
             :span="3"
             v-for="(col, index) in row"
             :key="index"
             @click="changeSelectProjectAction(col.type)"
-            class="col p-2  mr-5 text-center"
+            class="col mr-5 text-center"
             :class="{ active: currentSelect == col.type }"
           >
-            {{ col.name }}
+            <span>{{col.name}}</span>
           </a-col>
         </a-row>
       </div>
 
-      <div class="logoList ">
+      <div class="logoList">
         <ul class="clearfix font18">
-          <li
-            v-for="(item, index) in projectList[currentSelect]"
-            :key="index"
-            @click="gotodetails"
-          >
-            {{ item.name }}
+          <li v-for="(item, index) in projectList[currentSelect]" :key="index">
+            <a-button type="primary" class="look-details" @click="gotodetails">查看详情</a-button>
+            <img
+              v-lazy="require('../../assets/img/project-li-cover.png')"
+              alt
+              style="width:100%;height:100%;"
+            />
+            <span class="logoList text">{{ item.name }}</span>
           </li>
         </ul>
 
@@ -52,10 +48,10 @@ export default {
           { name: "全部案例", type: "all" },
           { name: "品牌VI设计", type: "vi" },
           { name: "APP开发", type: "app" },
-          { name: "海报设计", type: "pt" },
           { name: "LOGO设计", type: "logo" }
         ],
         [
+          { name: "全部案例", type: "all" },
           { name: "画册设计", type: "pa" },
           { name: "包装设计", type: "pg" },
           { name: "网站开发", type: "web" }
@@ -74,6 +70,7 @@ export default {
         .get("caselist")
         .then(res => {
           this.projectList = res;
+          console.log(res);
           this.sortAllProject();
         })
         .catch(err => {});
@@ -82,7 +79,7 @@ export default {
     sortAllProject() {
       let projectList = {};
 
-      ["app", "logo", "pa", "pg", "pt", "vi", "web"].map(type => {
+      ["app", "logo", "pa", "pg", "vi", "web"].map(type => {
         projectList[type] = this.projectList.filter(item => {
           return item.type == type;
         });
@@ -97,7 +94,7 @@ export default {
     },
     lookMore() {},
     gotodetails() {
-      this.$router.push({ name: "details" });
+      this.$router.push({ name: "details",query: { id: 1 } });
     }
   }
 };
@@ -120,6 +117,7 @@ export default {
     cursor: pointer;
   }
   .logoList {
+    margin-top: 40px;
     .lookMore {
       .lookMore-btn {
         width: 200px;
@@ -128,24 +126,69 @@ export default {
     }
     > ul {
       li {
+        position: relative;
+        top: -74px;
         float: left;
         width: 24%;
-        height: 240px;
-        margin: 0 1% 20px 0;
+        height: 281px;
+        margin: 74px 1% 20px 0;
         background-color: #eee;
         color: blue;
+        text-align: center;
         cursor: pointer;
+        img {
+          &:hover {
+            opacity: 0.3;
+            transition: 2s;
+          }
+        }
+        .look-details {
+          position: absolute;
+          z-index: 99;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+          width: 109px;
+          margin: auto;
+          opacity: 0;
+        }
+        &:hover .look-details {
+          opacity: 1;
+          transition: 2s;
+        }
+        span {
+          display: inline-block;
+          margin-top: 18px;
+          font-size: 18px;
+          color: #333333;
+        }
       }
     }
   }
 
   .select-project {
-    // width: 80%;
-    // margin: 0 auto;
+    height: 90px;
+    margin-top: 40px;
+    .ant-row-flex:last-child {
+      margin-top: 30px;
+      .col:first-child {
+        opacity: 0;
+      }
+    }
     .col {
+      width: 160px;
+      height: 30px;
+      font-size: 20px;
+      color: #333333;
+      border-radius: 5px;
+      background: #ffffff;
+      box-shadow: 0px 0px 10px -5px rgba(0, 0, 0);
       cursor: pointer;
       &.active {
-        background: rgb(201, 201, 201);
+        color: #ffffff;
+        background-color: #348ccd;
+        border-radius: 5px;
         transition: background 0.5s;
         font-weight: bold;
       }
