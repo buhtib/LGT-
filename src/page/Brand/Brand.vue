fdbd1a197c7994efdbad4c32a636c197019c6b5c
buhtib <2418531916@qq.com>
2019-09-17 20:46:42 +0800

    update

--- src/page/Brand/Brand.vue
+++ src/page/Brand/Brand.vue
@@ -11,9 +11,9 @@
           type="flex"
           justify="space-between"
         >
-          <a-col :span="8" v-for="(col, index) in row" :key="index" class="col">
+          <a-col :span="8" v-for="(col, index) in row" :key="index" class="col border">
             <div class="col_img">
-              <img v-lazy="require('../../assets/img/brand_touxiang.png')" alt style="height:200px;" />
+              <img v-lazy="col.img" alt style="height:200px;" />
             </div>
             <div class="col-list">
               <ul>
@@ -62,6 +62,7 @@ export default {
           col1: {
             name: "汤基彬",
             job: "项目主管",
+            img:require("../../assets/img/B.jpg"),
             intro_text_row1:
               "曾参与各大行业的网站开发，有着丰富的经验开发知识。用自身的专业挑战事业的最颠峰。",
             intro_text_row2: "LGT设计工作室创始人之一。"
@@ -69,6 +70,7 @@ export default {
           col2: {
             name: "卢广宗",
             job: "开发者",
+            img:require("../../assets/img/Z.jpg"),
             intro_text_row1:
               "曾在OPPO公司参与多项开发的项目，是中国互联网的年轻一代开发者。",
             intro_text_row2: "LGT设计工作室创始人之一。"
@@ -76,6 +78,7 @@ export default {
           col3: {
             name: "李嘉诚",
             job: "开发者",
+            img:require("../../assets/img/L.jpg"),
             intro_text_row1:
               "担任多家公司的项目负责人兼以及负责公司项目开发，也在各大行业中参与其开发。",
             intro_text_row2: "LGT设计工作室创始人之一。"
@@ -85,6 +88,7 @@ export default {
           col1: {
             name: "汤少清",
             job: "设计总监",
+            img:require("../../assets/img/Q.jpg"),
             intro_text_row1:
               "曾从事不同行业的设计工作，2016年开始参与内地的品牌设计业务。",
             intro_text_row2: "LGT设计工作室创始人之一。"
@@ -92,6 +96,7 @@ export default {
           col2: {
             name: "叶国凤",
             job: "品牌设计",
+            img:require("../../assets/img/Y.jpg"),
             intro_text_row1:
               "曾参与各大行业的品牌设计以及品牌定位升级，服务近百个品牌的项目。",
             intro_text_row2: "LGT设计工作室创始人之一。"
@@ -99,6 +104,7 @@ export default {
           col3: {
             name: "汤少萍",
             job: "HR负责人",
+            img:require("../../assets/img/P.jpg"),
             intro_text_row1:
               "主要接待客户并解决客户的要求问题，品牌知识资深的HR，曾接待来自不同行业的客户。",
             intro_text_row2: "LGT设计工作室创始人之一。"
@@ -117,6 +123,7 @@ export default {
       margin-bottom: 40px;
       .col {
         width: 20%;
+        padding: 0 10px;
         .col_img {
           text-align: center;
         }
